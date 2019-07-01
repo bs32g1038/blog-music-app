1604f9bca322551dee60a28eb2b1cd8dc83f3189
bs32g1038 <bs32g1038@163.com>
2019-07-01 04:23:20 -0700

    chore: adjust config

--- scripts/release-docker-image.sh
+++ scripts/release-docker-image.sh
@@ -2,6 +2,6 @@ set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -t bs32g1038/blog-music-app:${TRAVIS_TAG} .
+docker build -t lcnotes/blog-music-app:${TRAVIS_TAG} .
 
-docker push bs32g1038/blog-music-app:${TRAVIS_TAG}
+docker push lcnotes/blog-music-app:${TRAVIS_TAG}
